0b59a0e70f0d941d06657630c48add4a5be69a4b
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-09-20 14:29:59 +0530

    Inject Laptop dependency into Alien using @Autowired

---
+++ Springbootdemo/src/main/java/com/example/Springbootdemo/Laptop.java
@@ -0,0 +1,10 @@
+package com.example.Springbootdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void Compile(){
+        System.out.println("Compiling.......");
+    }
+}
