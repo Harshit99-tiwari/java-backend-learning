7a0e47cc1df1b0a7df7f66de838da4b934f2a07a
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-09-20 14:01:57 +0530

    Implement Spring dependency injection

---
+++ Springbootdemo/src/main/java/com/example/Springbootdemo/Alien.java
@@ -0,0 +1,10 @@
+package com.example.Springbootdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+    public void code(){
+        System.out.println("Coding.......");
+    }
+}
--- Springbootdemo/src/main/java/com/example/Springbootdemo/SpringbootdemoApplication.java
+++ Springbootdemo/src/main/java/com/example/Springbootdemo/SpringbootdemoApplication.java
@@ -2,13 +2,15 @@ package com.example.Springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootdemoApplication.class, args);
-		System.out.println("hello");
+	ApplicationContext context =  SpringApplication.run(SpringbootdemoApplication.class, args);
+	Alien obj = context.getBean(Alien.class);
+	obj.code();
 	}
 
 }
