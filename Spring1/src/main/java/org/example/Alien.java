e4de402c2ddd705df46df09756fae904dbf1177a
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-09-22 18:49:37 +0530

    Add object reference injection using Spring XML

--- Spring1/src/main/java/org/example/Alien.java
+++ Spring1/src/main/java/org/example/Alien.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Alien {
     private int age;
+    private Laptop lap;
     public Alien(){
         System.out.println("Alien object created");
     }
@@ -15,7 +16,16 @@ public class Alien {
         this.age = age;
     }
 
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
     public void code(){
         System.out.println("Alien coding");
+        lap.Compile();
     }
 }
---
+++ Spring1/src/main/java/org/example/Laptop.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Laptop
+{
+    public Laptop(){
+        System.out.println("Laptop object created");
+    }
+    public void code(){
+        System.out.println("laptop coding");
+    }
+    public void Compile(){
+        System.out.println("Laptop Compiling");
+    }
+}
