d8d850222a3d59f4ea1c03b288adb572fbe1c87b
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-09-23 17:33:12 +0530

    Configure constructor injection with type, name and index

--- Spring1/src/main/java/org/example/Alien.java
+++ Spring1/src/main/java/org/example/Alien.java
@@ -1,10 +1,18 @@
 package org.example;
 
+import java.beans.ConstructorProperties;
+
 public class Alien {
     private int age;
     private Laptop lap;
     public Alien(){
         System.out.println("Alien object created");
+    }
+    @ConstructorProperties({"age","lap"})
+    public Alien(int age,Laptop lap) {
+        System.out.println("Para alien constructor");
+        this.age = age;
+        this.lap = lap;
     }
 
     public int getAge() {
